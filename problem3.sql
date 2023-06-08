b269a043c9bd1f4d2e122bb464bb9dd1e6801491
tips4you <40390368+tips4you@users.noreply.github.com>
2023-06-08 01:39:24 -0400

    Update problem3.sql

--- problem3.sql
+++ problem3.sql
@@ -24,4 +24,22 @@ WITH RECURSIVE employee_hierarchy  AS (
     JOIN `composed-slice-383418.demo.employees_details` eh ON e.manager_ids = eh.manager_id
 )
 SELECT * FROM employee_hierarchy order by id,manager_ids;
+
+--Alternate Approach
+
+SELECT e.id, e.name, eh.manager_ids
+    FROM ( SELECT id, name, id as manager_ids
+    FROM `composed-slice-383418.demo.employees_details` WHERE id=1) eh
+    JOIN `composed-slice-383418.demo.employees_details` e ON e.manager_id = eh.id
+
+SELECT * FROM employee_hierarchy ORDER BY manager_ids, id;
 --
+WITH RECURSIVE employee_hierarchy AS (
+    SELECT id, name, id as manager_ids
+    FROM `composed-slice-383418.demo.employees_details`
+    UNION ALL
+    SELECT e.id, e.name, eh.manager_ids
+    FROM employee_hierarchy eh
+    JOIN `composed-slice-383418.demo.employees_details` e ON e.manager_id = eh.id
+)
+SELECT * FROM employee_hierarchy ORDER BY manager_ids, id;
