6d4bd1609d03221aaf838bc9e93b1076da7f41a4
tips4you <40390368+tips4you@users.noreply.github.com>
2023-06-08 01:18:36 -0400

    Create problem3.sql

---
+++ problem3.sql
@@ -0,0 +1,27 @@
+--For each manager, show all the employee's working under them(including themselves)
+
+--STEP 1
+SELECT e.id, e.name, eh.id as manager_ids
+    FROM (SELECT id, name, id as manager_ids
+    FROM `composed-slice-383418.demo.employees_details` WHERE id=1) e
+    JOIN `composed-slice-383418.demo.employees_details` eh ON e.id = eh.manager_id
+
+--STEP 2
+SELECT e.id, e.name, eh.id as manager_ids
+    FROM (SELECT e.id, e.name, eh.id as manager_ids
+    FROM (SELECT id, name, id as manager_ids
+    FROM `composed-slice-383418.demo.employees_details` WHERE id=1) e
+    JOIN `composed-slice-383418.demo.employees_details` eh ON e.id = eh.manager_id) e
+    JOIN `composed-slice-383418.demo.employees_details` eh ON e.manager_ids = eh.manager_id
+
+--STEP 3
+WITH RECURSIVE employee_hierarchy  AS (
+    SELECT id, name, id as manager_ids
+    FROM `composed-slice-383418.demo.employees_details`
+    UNION ALL
+    SELECT e.id, e.name, eh.id as manager_ids
+    FROM employee_hierarchy e
+    JOIN `composed-slice-383418.demo.employees_details` eh ON e.manager_ids = eh.manager_id
+)
+SELECT * FROM employee_hierarchy order by id,manager_ids;
+--
