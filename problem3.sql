513ed8f37325c81b4a7828eb0f5026c074581bf2
tips4you <40390368+tips4you@users.noreply.github.com>
2023-06-21 10:44:18 -0400

    Update problem3.sql

--- problem3.sql
+++ problem3.sql
@@ -1,43 +1,28 @@
 --For each manager, show all the employee's working under them(including themselves)
 
 --STEP 1
-SELECT e.id, e.name, eh.id as manager_ids
-    FROM (SELECT id, name, id as manager_ids
-    FROM `composed-slice-383418.demo.employees_details` WHERE id=1) e
-    JOIN `composed-slice-383418.demo.employees_details` eh ON e.id = eh.manager_id
+SELECT e.id, e.name, m.id as associate_id
+FROM 
+    (SELECT id, name, id as associate_id
+    FROM `pathwaytips001.staging.employees_details` WHERE id=1) e
+JOIN `pathwaytips001.staging.employees_details` m 
+ON e.associate_id = m.manager_id
 
 --STEP 2
-SELECT e.id, e.name, eh.id as manager_ids
-    FROM (SELECT e.id, e.name, eh.id as manager_ids
-    FROM (SELECT id, name, id as manager_ids
-    FROM `composed-slice-383418.demo.employees_details` WHERE id=1) e
-    JOIN `composed-slice-383418.demo.employees_details` eh ON e.id = eh.manager_id) e
-    JOIN `composed-slice-383418.demo.employees_details` eh ON e.manager_ids = eh.manager_id
+SELECT e.id, e.name, m.id as associate_id
+FROM 
+    (SELECT id, name, id as associate_id
+    FROM `pathwaytips001.staging.employees_details`) e
+JOIN `pathwaytips001.staging.employees_details` m 
+ON e.associate_id = m.manager_id
 
 --STEP 3
 WITH RECURSIVE employee_hierarchy  AS (
-    SELECT id, name, id as manager_ids
-    FROM `composed-slice-383418.demo.employees_details`
+    SELECT id, name, id as associate_id
+    FROM `pathwaytips001.staging.employees_details`
     UNION ALL
-    SELECT e.id, e.name, eh.id as manager_ids
+    SELECT e.id, e.name, m.id as associate_id
     FROM employee_hierarchy e
-    JOIN `composed-slice-383418.demo.employees_details` eh ON e.manager_ids = eh.manager_id
+    JOIN `pathwaytips001.staging.employees_details` m ON e.associate_id = m.manager_id
 )
-SELECT * FROM employee_hierarchy order by id,manager_ids;
-
---Alternate Approach
-
-SELECT e.id, e.name, eh.manager_ids
-    FROM ( SELECT id, name, id as manager_ids
-    FROM `composed-slice-383418.demo.employees_details` WHERE id=1) eh
-    JOIN `composed-slice-383418.demo.employees_details` e ON e.manager_id = eh.id
---
-WITH RECURSIVE employee_hierarchy AS (
-    SELECT id, name, id as manager_ids
-    FROM `composed-slice-383418.demo.employees_details`
-    UNION ALL
-    SELECT e.id, e.name, eh.manager_ids
-    FROM employee_hierarchy eh
-    JOIN `composed-slice-383418.demo.employees_details` e ON e.manager_id = eh.id
-)
-SELECT * FROM employee_hierarchy ORDER BY manager_ids, id;
+SELECT * FROM employee_hierarchy order by id,associate_id;
